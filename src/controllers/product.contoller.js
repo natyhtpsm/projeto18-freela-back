bd1d90937da6ce3507de2630d12066bc089d0c9c
natyhtpsm <119613135+natyhtpsm@users.noreply.github.com>
2023-08-13 19:04:29 -0300

    Fix: token validation funcionando

--- src/controllers/product.contoller.js
+++ src/controllers/product.contoller.js
@@ -28,38 +28,38 @@ export async function getProduct(req, res) {
 }
 
 export async function postProduct(req, res) {
-    const id_seller = res.locals.user_id; 
+    const id_seller = res.locals.user_id;
     console.log('A4: ', id_seller);
-  
-    const { name, description, photo, category, status, price, } = req.body;
+
+    const { name, description, photo, category, status, price } = req.body;
     const query = 'INSERT INTO products (id_seller, name, description, photo, category, status, price) VALUES ($1, $2, $3, $4, $5, $6, $7)';
-  
+
     try {
-      const values = [id_seller, name, description, photo, category, status, price,];
-      await db.query(query, values);
-      res.send('Produto adicionado ao banco de dados.');
+        const values = [id_seller, name, description, photo, category, status, price];
+        await db.query(query, values);
+        res.send('Produto adicionado ao banco de dados.');
     } catch (error) {
-      return res.status(500).send(error.message);
+        return res.status(500).send(error.message);
     }
 }
 
 export async function getUserInfo(req, res) {
     try {
-        const userId = res.locals.id_seller;
-        console.log('LOCALS: ', res.locals);
-        if (!userId) return res.status(400).send('ID do usuário não encontrado em res.locals.');
-
-        const query = 'SELECT name, phone, email FROM users WHERE id = $1';
-        const values = [userId];
-        const result = await db.query(query, values);
-
-        if (result.rows.length === 0) return res.status(404).send('Usuário não encontrado.');
-
-        res.send(result.rows[0]);
+      const userId = res.locals.user_id; 
+      if (!userId) return res.status(400).send('ID do usuário não encontrado em res.locals.');
+  
+      const query = 'SELECT name, phone, email FROM users WHERE id = $1';
+      const values = [userId];
+      const result = await db.query(query, values);
+  
+      if (result.rows.length === 0) return res.status(404).send('Usuário não encontrado.');
+  
+      res.send(result.rows[0]);
     } catch (error) {
-        return res.status(500).send(error.message);
+      return res.status(500).send(error.message);
     }
-}
+  }
+  
 
 
 
