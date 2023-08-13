05c2008a05da7a36b35f83df174448fbea7802a3
natyhtpsm <119613135+natyhtpsm@users.noreply.github.com>
2023-08-13 19:29:37 -0300

    .

--- src/middlewares/tokenValidation.js
+++ src/middlewares/tokenValidation.js
@@ -2,9 +2,14 @@ import { db } from "../database/database.js";
 
 export async function tokenValidation(req, res, next) {
   const token = req.headers.authorization?.replace("Bearer ", "");
+
+  if (!token) {
+    return res.status(401).send('Token inválido.');
+  }
+
   const tokenArray = [token];
   const outputArray = tokenArray.map(item => item.replace(/'/g, ''));
-  if (!token) return res.status(401).send('Token inválido.');
+
   try {
     const result = await db.query('SELECT users.*, sessions.* FROM users JOIN sessions ON users.id = sessions.user_id WHERE token = $1', outputArray);
     if (result.rowCount === 0) return res.status(404).send('Usuário não encontrado.');
@@ -15,4 +20,5 @@ export async function tokenValidation(req, res, next) {
     return res.status(500).send(error.message);
   }
 }
+
 
