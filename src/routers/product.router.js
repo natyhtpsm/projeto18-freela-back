0dfdb40c3edea5c4eb42059f00b61da74bf0487d
natyhtpsm <119613135+natyhtpsm@users.noreply.github.com>
2023-08-13 17:48:58 -0300

    Feat: find user

--- src/routers/product.router.js
+++ src/routers/product.router.js
@@ -1,18 +1,13 @@
 import express from 'express';
-import { postProduct, getProduct, getProducts } from '../controllers/product.contoller.js';
+import { postProduct, getProduct, getProducts, getUserInfo } from '../controllers/product.contoller.js';
 import { schemaValidation } from '../middlewares/schemaValidation.js';
 import { postProductSchema } from '../schemas/product.schema.js';
 import { tokenValidation } from '../middlewares/tokenValidation.js';
-import { getUserInfoFromToken } from '../controllers/autenticacao.controller.js';
 
 const productRouter = express.Router();
 
 productRouter.post('/produto', tokenValidation, schemaValidation(postProductSchema), postProduct);
 productRouter.get('/produto/:id', getProduct);
 productRouter.get('/home', getProducts);
-productRouter.get('/user-info', getUserInfoFromToken, (req, res) => {
-    const userInfo = res.locals.user;
-    return res.send(userInfo);
-  });
-
+productRouter.get('/user-info/:id', getUserInfo);
 export default productRouter;
