512eaad0e2a303f8d5ae60088953db3b051f94fe
natyhtpsm <119613135+natyhtpsm@users.noreply.github.com>
2023-08-13 22:08:10 -0300

    Feat: get my products

--- src/controllers/product.contoller.js
+++ src/controllers/product.contoller.js
@@ -13,19 +13,61 @@ export async function getProducts(req, res) {
 export async function getProduct(req, res) {
     const { id } = req.params;
     if (!id) return res.status(400).send("O id do produto é obrigatório!");
-
+  
     try {
-        const query = 'SELECT * FROM products WHERE id = $1';
-        const values = [id];
-        const result = await db.query(query, values);
-
-        if (result.rows.length === 0) return res.status(404).send('Produto não encontrado.');
-
-        res.send(result.rows[0]);
+      const query = `
+        SELECT p.*, u.name as seller_name, u.phone as seller_phone, u.email as seller_email
+        FROM products p
+        JOIN users u ON p.id_seller = u.id
+        WHERE p.id = $1
+      `;
+      const values = [id];
+      const result = await db.query(query, values);
+  
+      if (result.rows.length === 0) return res.status(404).send('Produto não encontrado.');
+  
+      const productData = {
+        id: result.rows[0].id,
+        id_seller: result.rows[0].id_seller,
+        name: result.rows[0].name,
+        description: result.rows[0].description,
+        photo: result.rows[0].photo,
+        category: result.rows[0].category,
+        status: result.rows[0].status,
+        phone_seller: result.rows[0].phone_seller,
+        price: result.rows[0].price
+      };
+  
+      const sellerData = {
+        id: result.rows[0].id_seller,
+        name: result.rows[0].seller_name,
+        phone: result.rows[0].seller_phone,
+        email: result.rows[0].seller_email
+      };
+  
+      res.send({ product: productData, seller: sellerData });
     } catch (error) {
-        return res.status(500).send(error.message);
+      return res.status(500).send(error.message);
     }
 }
+
+export async function getMyProducts(req, res) {
+  const userId = res.locals.user_id; 
+  if (!userId) {
+    return res.status(400).send('ID do usuário não encontrado em res.locals.');
+  }
+
+  try {
+    const query = 'SELECT * FROM products WHERE id_seller = $1';
+    const values = [userId];
+    const result = await db.query(query, values);
+
+    res.send(result.rows);
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+}
+
 
 export async function postProduct(req, res) {
     const id_seller = res.locals.user_id;
@@ -42,24 +84,6 @@ export async function postProduct(req, res) {
         return res.status(500).send(error.message);
     }
 }
-
-export async function getUserInfo(req, res) {
-    try {
-      const userId = res.locals.user_id; 
-      if (!userId) return res.status(400).send('ID do usuário não encontrado em res.locals.');
-  
-      const query = 'SELECT name, phone, email FROM users WHERE id = $1';
-      const values = [userId];
-      const result = await db.query(query, values);
-  
-      if (result.rows.length === 0) return res.status(404).send('Usuário não encontrado.');
-  
-      res.send(result.rows[0]);
-    } catch (error) {
-      return res.status(500).send(error.message);
-    }
-  }
-  
 
 
 
--- src/routers/product.router.js
+++ src/routers/product.router.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { postProduct, getProduct, getProducts, getUserInfo } from '../controllers/product.contoller.js';
+import { postProduct, getProduct, getProducts, getMyProducts } from '../controllers/product.contoller.js';
 import { schemaValidation } from '../middlewares/schemaValidation.js';
 import { postProductSchema } from '../schemas/product.schema.js';
 import { tokenValidation } from '../middlewares/tokenValidation.js';
@@ -9,5 +9,5 @@ const productRouter = express.Router();
 productRouter.post('/produto', tokenValidation, schemaValidation(postProductSchema), postProduct);
 productRouter.get('/produto/:id', getProduct);
 productRouter.get('/home', getProducts);
-productRouter.get('/user-info', getUserInfo);
+productRouter.get('/meus-produtos', tokenValidation, getMyProducts);
 export default productRouter;
